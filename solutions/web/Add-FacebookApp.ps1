f82d9648484e0df7fd4dbcb84e62c67d65ec8667
dmitriy kataskin <dmtiriy.ataskin@gmail.com>
2013-07-25 16:20:44 +0400

    deploy website scenario

---
+++ solutions/web/Add-FacebookApp.ps1
@@ -0,0 +1,36 @@
+﻿param(
+    [Parameter(Mandatory = $true)]
+    [String]$WebSiteName,
+
+    [Parameter(Mandatory = $true)]
+    [String]$WebSiteLocation,
+
+    [Parameter(Mandatory = $true)]
+    [String]$SQLDatabaseUsername,
+
+    [Parameter(Mandatory = $true)]
+    [String]$SQLDatabasePassword,
+
+    [Parameter(Mandatory = $true)]
+    [String]$WebSiteProjectFilePath,
+
+    [Parameter(Mandatory = $true)]
+    [String]$FacebookAppId,
+
+    [Parameter(Mandatory = $true)]
+    [String]$FacebookSecretKey
+)
+
+$powershellSamplesRoot = "D:\Projects\3d party\azure-powershell-samples"
+
+& $powershellSamplesRoot\create-azure-website-env.ps1 -Name $WebSiteName -Location $WebSiteLocation `
+-SqlDatabaseUserName $SQLDatabaseUsername -SqlDatabasePassword $SQLDatabasePassword
+
+& $powershellSamplesRoot\deploy-azure-website-devbox.ps1 -ProjectFile $WebSiteProjectFilePath
+
+$settings = New-Object Hashtable
+$settings["FacebookAppId"] = $FacebookAppId
+$settings["FacebookAppSecret"] = $FacebookSecretKey
+
+Get-AzureWebsite -Name $WebSiteName | Set-AzureWebsite -AppSettings $settings
+
